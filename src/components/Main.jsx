aebe91bbe014444325bfc23ca4b3b1e468897a1f
Pavel Merekin <merekin33@gmail.com>
2025-03-16 19:22:21 +0700

    Initial settings and header

---
+++ src/components/Main.jsx
@@ -0,0 +1,3 @@
+export default function Main() {
+  return <h1>This is Main component</h1>
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header className="header">
+      <nav className="nav-list">
+        <img src="reactjs-icon.svg" alt="React Logo" className="logo-img" />
+        <span className="logo-text">ReactFacts</span>
+      </nav>
+    </header>
+  )
+}
